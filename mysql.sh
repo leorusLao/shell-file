d0326c506c6d7ace7320f7db01d9c8969b2079b4
leorusLao <hongxin_lao@126.com>
2018-09-13 12:16:53 +0800

    mysql install shell

---
+++ mysql.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+type wget >/dev/null 2>&1 || yum install -y wget
+
+echo '===start download mysql.rpm==='
+wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
+echo '===end download mysql.rpm==='
+
+rpm -ivh mysql-community-release-el7-5.noarch.rpm
+
+if [ $? -eq 0 ]
+then
+
+rm -f mysql-community-release-el7-5.noarch.rpm
+
+fi
+
+echo '===start install mysql==='
+yum install -y mysql-server
+echo '===end install mysql==='
+
+
+echo 'complete'
+
+
+
+
+
+
